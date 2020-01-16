1c21810f324efc1df8982ad63e6110aad6e47020
yasirkhan95 <55588816+yasirkhan95@users.noreply.github.com>
2020-01-16 18:03:28 +0500

    Updates

---
+++ precache-manifest.a90e3cd4a671a0126d0015fb1431a3da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9275af6c0aa4d923a5b773443abb08",
+    "url": "/pagination/index.html"
+  },
+  {
+    "revision": "53c08d6a96896beb8157",
+    "url": "/pagination/static/css/main.d55f7da5.chunk.css"
+  },
+  {
+    "revision": "5fa845613088556c48fd",
+    "url": "/pagination/static/js/2.a227c85d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/pagination/static/js/2.a227c85d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c08d6a96896beb8157",
+    "url": "/pagination/static/js/main.2256e21f.chunk.js"
+  },
+  {
+    "revision": "1749c72c74a93b9a2630",
+    "url": "/pagination/static/js/runtime-main.d0175ff5.js"
+  }
+]);
